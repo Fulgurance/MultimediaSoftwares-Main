2aaae4dfde4b7f23ca647ed61c0849872ba8ac3c
Fulgurance <zohran.londais@gmail.com>
2023-04-21 09:15:49 +0100

    Added openrc support

--- Alsa-Utils/1.2.5-r1/1.2.5-r1.cr
+++ Alsa-Utils/1.2.5-r1/1.2.5-r1.cr
@@ -20,6 +20,12 @@ class Target < ISM::Software
         super
 
         makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        if option("Openrc")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+            moveFile("#{workDirectoryPath(false)}/alsasound.initd-r8","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/alsasound")
+            runChmodCommand(["+x","alsasound"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/init.d")
+        end
     end
 
 end
