46eece9b8ef1fb3ed20f3cc9547705607d52e81b
Fulgurance <zohran.londais@gmail.com>
2024-05-14 19:58:48 +0100

    Added pipewire software

---
+++ Pipewire/0.3.77/0.3.77.cr
@@ -0,0 +1,32 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runMesonCommand([   "setup",
+                            "--reconfigure",
+                            @buildDirectoryNames["MainBuild"],
+                            "--prefix=/usr",
+                            "--buildtype=release",
+                            "-Dsession-managers=\"[]\""],
+                            mainWorkDirectoryPath)
+    end
+
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+end
