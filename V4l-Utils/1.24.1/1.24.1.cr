2d6410d323dba103a7084ccfa182d7fcce4b2e17
Fulgurance <zohran.londais@gmail.com>
2024-09-08 20:38:14 +0100

    Improved v4l-utils installation

--- V4l-Utils/1.24.1/1.24.1.cr
+++ V4l-Utils/1.24.1/1.24.1.cr
@@ -20,6 +20,16 @@ class Target < ISM::Software
 
         makeSource( arguments:      "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
                     path:           buildDirectoryPath)
+
+        if !option("Qv4l2")
+            deleteFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin/qv4l2")
+            deleteFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/share/applications/qv4l2.desktop")
+        end
+
+        if !option("Qvidcap")
+            deleteFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin/qvidcap")
+            deleteFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/share/applications/qvidcap.desktop")
+        end
     end
 
 end
