0777409f388ab8fbf3270f229907140529a5d609
Fulgurance <zohran.londais@gmail.com>
2024-11-11 17:09:46 +0000

    Added support for assembler optimization

--- Ffmpeg/7.1.0/7.1.0.cr
+++ Ffmpeg/7.1.0/7.1.0.cr
@@ -22,6 +22,7 @@ class Target < ISM::Software
                                     #{option("X264") ? "--enable-libx264" : ""}             \
                                     #{option("X265") ? "--enable-libx265" : ""}             \
                                     --enable-openssl                                        \
+                                    #{option("Nasm") || option("Yasm") ? "--enable-asm" : "--disable-asm"}  \
                                     --docdir=/usr/share/doc/#{versionName}",
                         path:       buildDirectoryPath)
     end
