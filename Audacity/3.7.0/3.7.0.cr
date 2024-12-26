4ed22657035f1c61c3cbf04ab835e4da9d5ac4d7
Fulgurance <zohran.londais@gmail.com>
2024-12-26 20:57:22 +0000

    Updated audacity configuration phase

--- Audacity/3.7.0/3.7.0.cr
+++ Audacity/3.7.0/3.7.0.cr
@@ -8,8 +8,15 @@ class Target < ISM::Software
     def configure
         super
 
-        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr    \
-                                    -DCMAKE_BUILD_TYPE=Release      \
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr        \
+                                    -DCMAKE_BUILD_TYPE=Release          \
+                                    -DAUDACITY_BUILD_LEVEL=2            \
+                                    -Daudacity_conan_enabled=off        \
+                                    -Daudacity_has_updates_check=off    \
+                                    -Daudacity_has_audiocom_upload=off  \
+                                    -Daudacity_has_sentry_reporting=off \
+                                    -Daudacity_has_crashreports=off     \
+                                    -Daudacity_has_tests=off            \
                                     ..",
                         path:       buildDirectoryPath)
     end
