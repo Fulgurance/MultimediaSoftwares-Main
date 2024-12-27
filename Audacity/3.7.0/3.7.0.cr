e492d80dd22af88f21b31f706e244677197d182b
Fulgurance <zohran.londais@gmail.com>
2024-12-27 18:48:35 +0000

    Updated installation process

--- Audacity/3.7.0/3.7.0.cr
+++ Audacity/3.7.0/3.7.0.cr
@@ -8,15 +8,46 @@ class Target < ISM::Software
     def configure
         super
 
-        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr        \
-                                    -DCMAKE_BUILD_TYPE=Release          \
-                                    -DAUDACITY_BUILD_LEVEL=2            \
-                                    -Daudacity_conan_enabled=off        \
-                                    -Daudacity_has_updates_check=off    \
-                                    -Daudacity_has_audiocom_upload=off  \
-                                    -Daudacity_has_sentry_reporting=off \
-                                    -Daudacity_has_crashreports=off     \
-                                    -Daudacity_has_tests=off            \
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr            \
+                                    -DCMAKE_BUILD_TYPE=Release              \
+                                    -DAUDACITY_BUILD_LEVEL=2                \
+                                    -Daudacity_has_networking=off           \
+                                    -Daudacity_conan_enabled=off            \
+                                    -Daudacity_has_updates_check=off        \
+                                    -Daudacity_has_audiocom_upload=off      \
+                                    -Daudacity_has_sentry_reporting=off     \
+                                    -Daudacity_has_crashreports=off         \
+                                    -Daudacity_has_tests=off                \
+                                    -Daudacity_lib_preference=system        \
+                                    -Daudacity_obey_system_dependencies=on  \
+                                    -Daudacity_use_expat=system             \
+                                    -Daudacity_use_ffmpeg=off               \
+                                    -Daudacity_use_libid3tag=off            \
+                                    -Daudacity_use_ladspa=off               \
+                                    -Daudacity_use_lame=system              \
+                                    -Daudacity_use_wxwidgets=system         \
+                                    -Daudacity_use_libflac=off              \
+                                    -Daudacity_use_libmp3lame=system        \
+                                    -Daudacity_use_libmpg123=off            \
+                                    -Daudacity_use_libogg=off               \
+                                    -Daudacity_use_libopus=off              \
+                                    -Daudacity_use_libsndfile=system        \
+                                    -Daudacity_use_libvorbis=off            \
+                                    -Daudacity_use_lv2=off                  \
+                                    -Daudacity_use_midi=system              \
+                                    -Daudacity_use_nyquist=local            \
+                                    -Daudacity_use_opusfile=off             \
+                                    -Daudacity_use_pch=off                  \
+                                    -Daudacity_use_portaudio=system         \
+                                    -Daudacity_use_portmixer=off            \
+                                    -Daudacity_use_portsmf=system           \
+                                    -Daudacity_use_rapidjson=system         \
+                                    -Daudacity_use_sbsms=off                \
+                                    -Daudacity_use_soundtouch=system        \
+                                    -Daudacity_use_soxr=system              \
+                                    -Daudacity_use_twolame=off              \
+                                    -Daudacity_use_vamp=off                 \
+                                    -Daudacity_use_wavpack=off              \
                                     ..",
                         path:       buildDirectoryPath)
     end
